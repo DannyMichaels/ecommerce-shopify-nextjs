8fa7061d747ce15f519fa50d885049a11518619c
Daniel <itzdanielmichael@gmail.com>
2022-02-27 11:06:15 -0500

    updates to env

--- shopify.ts
+++ shopify.ts
@@ -1,12 +1,12 @@
 import Client, { Image, Query } from 'shopify-buy';
 
-export const client = Client.buildClient({
-  domain: String(process.env.SHOPIFY_DOMAIN),
-  storefrontAccessToken: String(process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN),
-});
-
 const domain = process.env.SHOPIFY_DOMAIN;
 const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+
+export const client = Client.buildClient({
+  domain: process.env.SHOPIFY_DOMAIN as string,
+  storefrontAccessToken: storefrontAccessToken as string,
+});
 
 export async function ShopifyQuery(query: string) {
   const URL = `https://${domain}/api/2022-01/graphql.json`;
