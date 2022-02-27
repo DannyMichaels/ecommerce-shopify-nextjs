f019738b9f9fd3e728dcc350f6db320f2d034646
Daniel <itzdanielmichael@gmail.com>
2022-02-26 21:12:29 -0500

    update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
-import { Box, Flex, Text, Icon, Image as image2 } from '@chakra-ui/react';
+import { Flex, Icon } from '@chakra-ui/react';
 import { useStore } from '../store';
 import { MdMenu, MdShoppingBasket } from 'react-icons/md'; // material design icons
 import Image from './Image';
+import Link from 'next/link';
 
 export default function Navbar() {
-  const { setIsCartOpen, setIsMenuOpen, checkout } = useStore();
+  const { setIsCartOpen, setIsMenuOpen, checkout } = useStore((store) => ({
+    setIsCartOpen: store.setIsCartOpen,
+    setIsMenuOpen: store.setIsMenuOpen,
+    checkout: store.checkout,
+  }));
 
   return (
     <Flex
@@ -13,7 +18,12 @@ export default function Navbar() {
       justifyContent="space-between"
       p="2rem">
       <Icon fill="white" cursor="pointer" as={MdMenu} w={30} h={30} />
-      <Image src="/images/logo.svg" width={100} height={100} alt="Logo" />
+
+      <Link href="/" passHref>
+        <a>
+          <Image src="/images/logo.svg" width={100} height={100} alt="Logo" />
+        </a>
+      </Link>
 
       <Icon
         fill="white"
